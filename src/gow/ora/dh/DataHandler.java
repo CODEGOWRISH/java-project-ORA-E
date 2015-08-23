49ad543e5490cd8f9d3a6f43a03287faf6551ed0
dbgsm0 <gowrish.mallipattana@target.com>
2015-08-23 20:36:27 +0530

    new commit Sun Aug 23 20:36:27 IST 2015

--- src/gow/ora/dh/DataHandler.java
+++ src/gow/ora/dh/DataHandler.java
@@ -1,15 +1,39 @@
 package gow.ora.dh;
 
+import gow.ora.conn.DbConn;
+
 import java.sql.*;
 
 public class DataHandler {
-	
 
-	public ResultSet queryResultSet (Connection connection, String query) throws SQLException {
-		
-		Statement 	statement = connection.createStatement();
-		ResultSet	resultSet = statement.executeQuery(query);
-		
+	public ResultSet queryResultSet(Connection connection, String query)
+			throws SQLException {
+
+		Statement statement = connection.createStatement(
+				ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		ResultSet resultSet = statement.executeQuery(query);
+		resultSet.beforeFirst();
+
+		return resultSet;
+	}
+
+	public ResultSet queryResultSet(String username, String password,
+			String hostOrScanName, String port, String serviceNameOrSID,
+			String query) throws SQLException {
+
+		// For debug only
+		// String jdbcURL = "jdbc:oracle:thin:" + username + "/" + password +
+		// "@//" + hostOrScanName + ":" + port + "/" + serviceNameOrSID;
+		// System.out.println ("INFO - jdbcURL in DataHandler is - " + jdbcURL);
+
+		Connection connection = new DbConn().getDBConnJDBCThin(username,
+				password, hostOrScanName, port, serviceNameOrSID);
+
+		Statement statement = connection.createStatement(
+				ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		ResultSet resultSet = statement.executeQuery(query);
+		resultSet.beforeFirst();
+
 		return resultSet;
 	}
 
--- src/gow/ora/dh/TestJDBCThinConn.java
+++ src/gow/ora/dh/TestJDBCThinConn.java
@@ -5,41 +5,42 @@ import java.sql.SQLException;
 
 import gow.ora.conn.*;
 
-
 public class TestJDBCThinConn {
 
 	public static void main(String[] args) {
 
-		System.out.println ("INFO - Entering");
-		
+		System.out.println("INFO - Entering");
+
 		int argCount = args.length;
-		
-		
+
 		if (argCount < 5) {
-			System.out.println ("ERR- Insufficient Arguments.  Need username, password, hostOrScanName,"
-					+ " port, serviceName");
+			System.out
+					.println("ERR- Insufficient Arguments.  Need username, password, hostOrScanName,"
+							+ " port, serviceName");
 		}
-		
-		String 	username = args[0];
-		String 	password = args[1];
-		String	hostOrScanName = args[2];
-		String 	port = args[3];
-		String	serviceName = args[4];
-		
-		try
-		{
-			Connection connection = new DbConn().getDBConnJDBCThin (username, password, hostOrScanName, port, serviceName);
+
+		String username = args[0];
+		String password = args[1];
+		String hostOrScanName = args[2];
+		String port = args[3];
+		String serviceName = args[4];
+
+		try {
+			Connection connection = new DbConn().getDBConnJDBCThin(username,
+					password, hostOrScanName, port, serviceName);
+
+			// DBConn dbConnClass = new DbConn();
+			// Connection connection = dbConnClass.getDBConnJDBCThin (username,
+			// password, hostOrScanName, port, serviceName);
 			
-			//DBConn dbConnClass = new DbConn();
-			//Connection connection = dbConnClass.getDBConnJDBCThin (username, password, hostOrScanName, port, serviceName);
+			connection.close();
+			
+		} catch (SQLException e) {
+			// you handle the exception here
+			e.printStackTrace();
 		}
-			catch (SQLException e)
-			{
-				// you handle the exception here
-					e.printStackTrace();
-			}
-		
-		System.out.println ("INFO - Exiting");
+
+		System.out.println("INFO - Exiting");
 	}
 
 }
